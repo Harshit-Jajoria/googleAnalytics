9b95b6482c515dd053323dd1a0eeae38e40aa5c4
Harshit-Jajoria <harshitjajoria66@gmail.com>
2024-03-05 13:36:13 +0530

    add react ga4

---
+++ react-ga/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import ReactGA4 from 'react-ga4';
+
+const initializeGA4 = async () => {
+  const measurementId = '64657498';
+  try {
+    await ReactGA4.initialize(measurementId);
+  } catch (error) {
+    console.error('Failed to initialize GA4:', error);
+  }
+};
+
+const App = () => {
+  useEffect(() => {
+    initializeGA4();
+  }, []);
+
+  const handleClick = () => {
+    ReactGA4.event({
+      action: 'button_clicked',
+      category: 'User Interaction',
+      label: 'My Button Label',
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+};
+
+export default App;
